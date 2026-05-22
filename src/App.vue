b49003cb59fb26933a4b941c3a8362976dc65791
ezhpry <zephyr0063@icloud.com>
2026-05-22 23:00:47 +0800

    - 完成组件封装 - 添加fast-glob修复运行报错

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SvgIcon from './components/SvgIcon.vue';
+
+</script>
 
 <template>
   <h1 class="text-blue-500">You did it!</h1>
@@ -6,6 +9,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <SvgIcon name="arrow"></SvgIcon>
 </template>
 
 <style scoped></style>
